bc2f68a57f37886669d9cc40861f844fa9b113de
AnshMNSoni <anshsoni702@gmail.com>
2026-07-02 11:47:09 +0530

    update(build) on pypi

--- pythonstl/__init__.py
+++ pythonstl/__init__.py
@@ -8,7 +8,7 @@ This package provides clean, STL-compliant interfaces for common
 data structures while hiding implementation details from users.
 """
 
-__version__ = "1.1.7"
+__version__ = "1.1.8"
 __author__ = "PySTL Contributors"
 
 from pythonstl.facade.stack import stack
